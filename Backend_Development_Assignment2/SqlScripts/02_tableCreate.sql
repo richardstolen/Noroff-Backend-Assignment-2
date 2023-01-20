6b06a5ca8e5abb19ad9cc04c5ec9312ea5bc7b68
richardstolen <richstolen@gmail.com>
2023-01-20 14:52:52 +0100

    feat: Added 02_tableCreate.sql. See appendix A: 2)

---
+++ Backend_Development_Assignment2/SqlScripts/02_tableCreate.sql
@@ -0,0 +1,45 @@
+﻿
+/*******************************************************************************
+	Appendix A: 2)
+	Create a script that contains a statement
+	for creating the tables.
+	The tables should be:
+		Superhero [Id integer auto increment], [Name], [Alias], [Origin]
+		Assistant [Id integer auto increment], [Name]
+		Power	  [Id integer auto increment], [Name], [Description
+*******************************************************************************/
+
+CREATE TABLE Superhero (
+	Id INT IDENTITY (1, 1) PRIMARY KEY,
+	[Name] NVARCHAR(50)
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
