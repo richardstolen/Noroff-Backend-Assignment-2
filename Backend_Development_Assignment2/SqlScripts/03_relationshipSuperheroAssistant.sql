54baa29b9f180db969cd9d616c42c6c05bdd20f2
richardstolen <richstolen@gmail.com>
2023-01-20 15:24:35 +0100

    feat: Added 03_relationshipSuperheroAsssistant.sql file Appendix A: 3)

---
+++ Backend_Development_Assignment2/SqlScripts/03_relationshipSuperheroAssistant.sql
@@ -0,0 +1,26 @@
+﻿
+/*******************************************************************************
+	Appendix A: 3)
+	Create a script that contains a statement
+	for creating the relationship between Superhero and Assistant
+	
+	The- relationship should be:
+		- One Superhero can have multiple assistants, 
+		  one assistant has one superhero they assist.
+*******************************************************************************/
+
+USE SuperheroesDB;
+GO
+
+-- Add SuperheroId column
+ALTER TABLE [Assistant]
+ADD [SuperheroId] INT
+GO
+
+-- Create foreign key, linking [Assistant].[SuperheroId] with [Superhero].[Id]
+ALTER TABLE [Assistant]
+ADD CONSTRAINT FK_Superhero
+FOREIGN KEY ([SuperheroId]) REFERENCES [Superhero]([Id])
+GO
+
+
