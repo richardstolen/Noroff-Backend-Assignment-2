73c04e509786f02afaa4ab3375524f2eb12ecc45
richardstolen <richstolen@gmail.com>
2023-01-20 14:58:40 +0100

    feat: Added 02_tableCreate.sql. Appendix A: 2)

--- Backend_Development_Assignment2/SqlScripts/02_tableCreate.sql
+++ Backend_Development_Assignment2/SqlScripts/02_tableCreate.sql
@@ -6,40 +6,34 @@
 	The tables should be:
 		Superhero [Id integer auto increment], [Name], [Alias], [Origin]
 		Assistant [Id integer auto increment], [Name]
-		Power	  [Id integer auto increment], [Name], [Description
+		Power	  [Id integer auto increment], [Name], [Description]
 *******************************************************************************/
 
-CREATE TABLE Superhero (
-	Id INT IDENTITY (1, 1) PRIMARY KEY,
-	[Name] NVARCHAR(50)
+USE SuperheroesDB;
 
+-- Superhero table
+CREATE TABLE [Superhero] (
+	[Id] INT IDENTITY (1, 1) PRIMARY KEY,
+	[Name] NVARCHAR(50) UNIQUE NOT NULL,
+	[Alias] NVARCHAR(50) NOT NULL,
+	[Origin] NVARCHAR(50) NOT NULL
 );
+GO
 
+-- Assistant table
+CREATE TABLE [Assistant] (
+	[Id] INT IDENTITY (1, 1) PRIMARY KEY,
+	[Name] NVARCHAR(50) UNIQUE NOT NULL
+);
+GO
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- Power table
+CREATE TABLE [Power] (
+	[Id] INT IDENTITY (1, 1) PRIMARY KEY,
+	[Name] NVARCHAR(50) UNIQUE NOT NULL,
+	[Description] NVARCHAR(60) NOT NULL
+);
+GO
 
 
 
