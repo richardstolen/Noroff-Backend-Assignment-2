e1e15aa89b0750205f68368ed0f1f7905b7a4402
richardstolen <richstolen@gmail.com>
2023-01-20 13:11:46 +0100

    Add project files.

---
+++ Backend_Development_Assignment2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Development_Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
