798c218f44b3a61dc4c2f0f0da50a7f00ea8b0ac
richardstolen <richstolen@gmail.com>
2023-01-20 13:38:24 +0100

    feat: Added 01_dbCreate.sql file. See Appendix A: 1)

---
+++ Backend_Development_Assignment2/SqlScripts/01_dbCreate.sql
@@ -0,0 +1,18 @@
+﻿
+/****************************************************************
+	Appendix A: 1)
+	Create a script that contains a statement
+	for creating a database.
+	The database should be called SuperheroesDB
+****************************************************************/
+
+-- Drop database if exists
+IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = 'SuperheroesDB')
+BEGIN
+	DROP DATABASE SuperheroesDB
+END
+
+-- Create database
+
+CREATE DATABASE SuperheroesDB;
+GO
