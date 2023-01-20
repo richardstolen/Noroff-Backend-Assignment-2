f7ff957b3bf34bea86b0cd1ebc47b2ba83b622ca
richardstolen <richstolen@gmail.com>
2023-01-20 15:55:06 +0100

    feat: Added 04_relationshipSuperheroPower.sql. Appendix A: 3)

---
+++ Backend_Development_Assignment2/SqlScripts/04_relationshipSuperheroPower.sql
@@ -0,0 +1,22 @@
+﻿
+/**********************************************************************************************************
+	Appendix A: 3)
+	Create a script that contains a statement
+	for creating the relationship between Superhero and Powers
+	
+	The- relationship should be:
+		One Superhero can have many powers, and one power can be present on many Superheroes
+		Many to many
+*********************************************************************************************************/
+
+USE SuperheroesDB;
+GO
+
+-- Creating a junction table for many-to-many relationship
+CREATE TABLE [Superhero_Power_Mapping](
+	[SuperheroId] INT REFERENCES [Superhero]([Id]),
+	[PowerId] INT REFERENCES [Power]([Id]),
+	PRIMARY KEY ([SuperheroId], [PowerId])
+);
+GO
+
