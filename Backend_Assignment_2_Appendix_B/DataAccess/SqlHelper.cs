accc19d45b7a4e02773a373e0ebac3c1497ac876
richardstolen <richstolen@gmail.com>
2023-01-23 10:34:27 +0100

    feat: Added customer class

---
+++ Backend_Assignment_2_Appendix_B/DataAccess/SqlHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Assignment_2_Appendix_B.DataAccess
+{
+    internal class SqlHelper
+    {
+    }
+}
---
+++ Backend_Assignment_2_Appendix_B/Model/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Assignment_2_Appendix_B.Model
+{
+    internal class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
