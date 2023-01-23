34817f58387313fdd9f72e0e3b80ddf09bc756cf
richardstolen <richstolen@gmail.com>
2023-01-23 08:50:14 +0100

    feat: Added 06_insertAssistants.sql

--- Backend_Development_Assignment2/SqlScripts/06_insertAssistants.sql
+++ Backend_Development_Assignment2/SqlScripts/06_insertAssistants.sql
@@ -2,17 +2,17 @@
 /**********************************************************************************************************
 	Appendix A: 4)
 	Create a script that contains a statement
-	for inserting new Superheroes into the database
+	for inserting new Assistants into the database
 	
 *********************************************************************************************************/
 
 USE SuperheroesDB;
 GO
 
--- Inserting new Superheroes
-INSERT INTO [Superhero] ([Name], [Alias], [Origin]) VALUES 
-	('Per Persen', 'Spider-man', 'Bit by spider'),
-	('Pål Pålsen', 'Ant-man', 'Bit by ant'),
-	('Espen Espensen', 'Rock-man', 'Hit by rock');
+-- Inserting new Assistants
+INSERT INTO [Assistant] ([Name], [SuperheroId]) VALUES 
+	('Frank Frankesen', 1),
+	('Katrine Katrinesen', 2),
+	('Petter Pettersen', 3);
 GO
 
