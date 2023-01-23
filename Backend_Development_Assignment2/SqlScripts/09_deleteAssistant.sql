203c7001ad8b5ca67f3f8060c061bf0412fa1408
richardstolen <richstolen@gmail.com>
2023-01-23 09:05:17 +0100

    feat: Added 09_deleteAssistant.sql

---
+++ Backend_Development_Assignment2/SqlScripts/09_deleteAssistant.sql
@@ -0,0 +1,16 @@
+﻿
+/**********************************************************************************************************
+	Appendix A: 5)
+	Create a script that contains a statement
+	for deleting a assistant
+	
+*********************************************************************************************************/
+
+USE SuperheroesDB;
+GO
+
+-- Updates superhero with id 1 - Spider man
+DELETE FROM [Assistant]
+WHERE [Name] = 'Petter Pettersen';
+GO
+
