cdda01b5dc64f965371f6242571a1f73375fc735
richardstolen <richstolen@gmail.com>
2023-01-23 10:35:59 +0100

    feat: Added sqlhelper

--- Backend_Assignment_2_Appendix_B/DataAccess/SqlHelper.cs
+++ Backend_Assignment_2_Appendix_B/DataAccess/SqlHelper.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
-namespace Backend_Assignment_2_Appendix_B.DataAccess
+namespace LessionTasks.Lession8
 {
     internal class SqlHelper
     {
+        public static string connectionString()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "DESKTOP-SQ77T5G\\SQLEXPRESS";
+            builder.InitialCatalog = "Chinook";
+            builder.IntegratedSecurity = true;
+            builder.TrustServerCertificate = true;
+
+            return builder.ConnectionString;
+        }
     }
 }
