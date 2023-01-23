61aada4182f908319ea3b0e2c6f99636ed89907d
richardstolen <richstolen@gmail.com>
2023-01-23 08:56:14 +0100

    feat: Added 07_powers.sql

---
+++ Backend_Development_Assignment2/SqlScripts/07_powers.sql
@@ -0,0 +1,19 @@
+﻿
+/**********************************************************************************************************
+	Appendix A: 4)
+	Create a script that contains a statement
+	for inserting new Powers into the database
+	
+*********************************************************************************************************/
+
+USE SuperheroesDB;
+GO
+
+-- Inserting new Assistants
+INSERT INTO [Power] ([Name], [Description]) VALUES 
+	('Shoot webs', 'Shoot webs that stick to enemies'),
+	('Shrink', 'Shrinks to an ant'),
+	('Throw rock', 'Throws rocks'),
+	('Fly', 'Flies in the air');
+GO
+
