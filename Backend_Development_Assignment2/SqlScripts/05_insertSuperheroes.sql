b080934ac60d6fb9a74813d77659647086534acc
richardstolen <richstolen@gmail.com>
2023-01-23 08:41:44 +0100

    feat: Added 05_insertSuperheroes.sql

---
+++ Backend_Development_Assignment2/SqlScripts/05_insertSuperheroes.sql
@@ -0,0 +1,20 @@
+﻿
+/**********************************************************************************************************
+	Appendix A: 4)
+	Create a script that contains a statement
+	for inserting new Superheroes into the database
+	
+	The- relationship should be:
+		One Superhero can have many powers, and one power can be present on many Superheroes
+		Many to many
+*********************************************************************************************************/
+
+USE SuperheroesDB;
+GO
+
+-- Inserting new Superheroes
+INSERT INTO [Superhero] ([Name], [Alias], [Origin]) VALUES 
+	('Per Persen', 'Spider-man', 'Bit by spider'),
+	('Pål Pålsen', 'Ant-man', 'Bit by ant');
+GO
+
